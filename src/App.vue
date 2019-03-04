8d9c70695e7d0b1cc65927ac4035d5e525712f96
Sergey <ser.ser.05021997@gmail.com>
2019-03-05 00:59:11 +0300

    add routes

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,44 @@
       .navbar
         .container
           .navbar-content
-            a.header-logo(href='') Film library
-            .button-burger
+            router-link.header-logo(
+              to="/"
+            ) Film library
+            .button-burger(
+              @click="menuShow = !menuShow"
+              :class="{ active: menuShow}"
+            )
               span.line.line-1
               span.line.line-2
               span.line.line-3
-            .navbar-list__wrapper
+            .navbar-list__wrapper(
+              :class="{ active: menuShow}"
+            )
               ul.navbar-list
-                li.navbar-item
-                  a.navbar-link(href='') Documetation
-
-    .content-wrapper
+                li.navbar-item(
+                  v-for="link in linkMenu"
+                  :key="link.title"
+                  @click="menuShow = false"
+                )
+                  router-link.navbar-link(
+                    :to="`${link.url}`"
+                  ) {{ link.title }}
+    router-view
 </template>
 
 <script>
-
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      menuShow: false,
+      linkMenu: [
+        {title: 'Home', url: '/'},
+        {title: 'Login', url: '/login'},
+        {title: 'Registration', url: '/registration'}
+      ]
+    }
+  }
 }
 </script>
 
---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+  .content-wrapper
+    section
+      .container
+        h1.ui-title-1 Home
+    section
+      .container
+        .tsk-list
+          .task-item(
+            v-for="task in tasks"
+            :key="task.id"
+            :class="{ completed: task.completed }"
+          )
+            .ui-card.ui-card--shadow
+              .task-item__info
+                .task-item__main-info
+                  span.ui-label.ui-label--light {{ task.whatWatch }}
+                  span Total Time:
+                span.button-close
+              .task-item__content
+                span.ui-title-3 {{ task.title }}
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tasks: [
+        {
+          'id': 1,
+          'title': 'GrawthBusters: Hooked on Grawth',
+          'description': 'GrawthBusters: Hooked on Grawth GrawthBusters: Hooked on Grawth',
+          'whatWatch': 'Film',
+          'completed': false,
+          'editiong': false
+        },
+        {
+          'id': 2,
+          'title': 'GrawthBusters: Hooked on Grawth',
+          'description': 'GrawthBusters: Hooked on Grawth GrawthBusters: Hooked on Grawth',
+          'whatWatch': 'Serial',
+          'completed': false,
+          'editiong': false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.task-item
+  margin-bottom 20px
+  &:last-child
+    margin-bottom 0
+.ui-label
+  margin-right 8px
+.task-item__info
+  display flex
+  align-items center
+  justify-content space-between
+  margin-bottom 20px
+  .button-close
+    width 20px
+    height @width
+</style>
